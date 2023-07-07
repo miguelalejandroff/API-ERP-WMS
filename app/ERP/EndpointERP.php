6d7f4f5f6db3415aade8f7628bd700d9dffaccec
miguelalejandroff@gmail.com <informatica@cals.cl>
2023-07-07 13:40:56 -0400

    este fue un cambio de prueba

--- app/ERP/EndpointERP.php
+++ app/ERP/EndpointERP.php
@@ -14,6 +14,10 @@ class EndpointERP
     public function __construct(public Request $request)
     {
         //mi cambio brutal nunca lo puedo enviar pipipi
+        /**
+         * 
+         * otro cambio mas brutal aun
+         */
     }
     public function createOrdenEntrada(ERPOrdenEntradaService $orden)
     {
